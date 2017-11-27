334c7099675c8275a1d72a5e45f5b33b970e30c7
admin <dangtinhtong@gmail.com>
2017-11-27 10:38:42 +0700

    first commit

--- Prog6.4/main.m
+++ Prog6.4/main.m
@@ -5,13 +5,26 @@
 //  Created by admin on 11/27/17.
 //  Copyright © 2017 admin. All rights reserved.
 //
+// program to determine if a number is even or odd
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
+        
+        int number_to_test, remainder;
+        NSLog(@"Enter your number to be tested: ");
+        scanf("%i",&number_to_test);
+        remainder = number_to_test%2;
+        if(remainder==0)
+        {
+            NSLog(@"The number is even.");
+        }else
+        {
+            NSLog(@"The number is odd.");
+        }
     }
     return 0;
 }
